b979130c9a2de48b3e03d804f94a3d59508324a2
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-04 08:30:11 +0530

    Creating the backend

---
+++ server/schema/schema.js
@@ -0,0 +1,34 @@
+const graphql=require('graphql')
+
+const {GraphQLObjectType,GraphQLString,GraphQlSchema}=graphql;
+
+const books = [
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+]
+const BookType=new GraphQLObjectType({
+    name:'Book',
+    fields:()=>({
+      id:{type:GraphQLString},
+      name:{type:GraphQLString},
+      genre:{type:GraphQLString}
+    })
+})
+
+const RootQuery=new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        book:{
+            type:BookType,
+            args:{id:{type:GraphQLString}},
+            resolve(parent,args){
+                args.id
+            }
+        }
+    }
+})
+
+module.exports= new GraphQlSchema({
+    query:RootQuery
+})
