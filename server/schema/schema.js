7ca66a2f3e2535e0f7856c7a19b25099324ee370
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-05 20:01:40 +0530

    should have made a gitignore file

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,18 +1,45 @@
-const graphql=require('graphql')
-
-const {GraphQLObjectType,GraphQLString,GraphQlSchema}=graphql;
+const graphql = require('graphql');
+const _ = require('lodash');
+const {GraphQLObjectType,
+    GraphQLString,
+    GraphQLID,
+    GraphQLInt,
+    GraphQLSchema}=graphql;
 
 const books = [
-  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
-  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+  {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+  {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+  {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+  {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
 ]
+
+const authors =  [
+  {name: 'Patrick Rothfuss', age: 44, id:"1"},
+  {name: 'Brandon Sanderson', age: 42, id:"2"},
+  {name: 'Terry Pratchett', age: 66, id:"3"},
+]
+const AuthorType=new GraphQLObjectType({
+    name:'Author',
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        age:{type:GraphQLInt}
+    })
+})
 const BookType=new GraphQLObjectType({
     name:'Book',
     fields:()=>({
-      id:{type:GraphQLString},
+      id:{type:GraphQLID},
       name:{type:GraphQLString},
-      genre:{type:GraphQLString}
+      genre:{type:GraphQLString},
+      author:{
+          type:AuthorType,
+          resolve(parent,args){
+              return _.find(authors,{id:parent.authorId})
+          }
+      }
     })
 })
 
@@ -21,14 +48,21 @@ const RootQuery=new GraphQLObjectType({
     fields:{
         book:{
             type:BookType,
-            args:{id:{type:GraphQLString}},
+            args:{id:{type:GraphQLID}},
             resolve(parent,args){
-                args.id
+               return _.find(books,{id:args.id})
+            }
+        },
+        author:{
+            type:AuthorType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return _.find(authors,{id:args.id})
             }
         }
     }
 })
 
-module.exports= new GraphQlSchema({
+module.exports= new GraphQLSchema({
     query:RootQuery
 })
